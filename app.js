2d05f570ef748ee03a081493f4c5b4974e526b6e
cerwyn <cerwyneliata.c@gmail.com>
2019-10-30 09:26:08 +0700

    Main Refractoring

--- app.js
+++ app.js
@@ -3,20 +3,19 @@ import express from 'express';
 var cron = require('node-cron');
 const app = express();
 const server = require('http').createServer(app);
-const bodyParser = require('body-parser');
 const io = require('socket.io').listen(server);
+
 const PORT = 3002;
 server.listen(PORT);
-
 console.log('Server is Running on Port '+PORT);
 
 const connections = [];
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-
+//Namespace
 const nsp = io.of('/room');
 
+  //Server will automatically emit/send message every 10 sec,
+  //to test if the message is delivered to the Flutter App.
   cron.schedule('*/10 * * * * *', () => {
     var dt = new Date();
     nsp.emit('new_message', {date: dt.getHours()+':'+dt.getMinutes()+':'+dt.getSeconds(), msg: 'Hello:) Message from server every 10 sec', id: 'server'});
